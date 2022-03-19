a6e448e720b410ef1b9c12d91628603e63a24664
Josue Lubaki <josuelubaki@gmail.com>
2022-03-19 12:15:26 -0400

    Initial commit

---
+++ src/main/java/ca/ghostteam/springulart/SpringulartApplication.java
@@ -0,0 +1,13 @@
+package ca.ghostteam.springulart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringulartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringulartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/ghostteam/springulart/SpringulartApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ghostteam.springulart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringulartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
